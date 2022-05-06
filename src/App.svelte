3bbfc0db7be1c94707a48a2bbdcc345f7c483eb0
ruimgbarros <ruimgbarros@gmail.com>
2022-05-06 23:28:46 +0100

    Initial state of app.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,23 @@
 <script>
 	import Tasks from "./Tasks.svelte";
+	import { csv } from "d3-fetch";
+
+
+	// To make our life easier, I've already imported the data. 
+	// First we say that data is null
+	let data = null;
+	// Then we fetch the data (as res) and assign it to data
+	csv("data.csv").then((res) => {
+  		data = res;
+	});
+
+
 </script>
 
 <main>
+
 	<!-- Just a simple tasks component with everything we will be doing -->
 	<Tasks />
+	
 </main>
 
