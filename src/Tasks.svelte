6eec89bbc39f60d34a4adf7879810f46ff6f8a74
ruimgbarros <ruimgbarros@gmail.com>
2022-05-21 13:21:11 +0200

    fixes

--- src/Tasks.svelte
+++ src/Tasks.svelte
@@ -9,8 +9,8 @@
 			{text: 'Make a new component called People. Pass all the data as a prop inside of it. Import it to app.svelte', status: false},
 			{text: 'Make a card for every single person in the database (hint: you can use the Card component already created)', status: false},
 			{text: 'Make cards show up side by side with css (or simply wrap all your cards with a div with "class="grid grid-cols-6 gap-2")', status: false},
-			{text: 'Extra: Can you make every single card slowly enter the page?")', status: false},
-			{text: 'Extra: Can you name an input menu that filters the cards by organization?")', status: false},
+			{text: 'Extra: Can you make every single card slowly enter the page?', status: false},
+			{text: 'Extra: Can you create an input menu that filters the cards by organization?', status: false},
 
 		];
 	
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import App from './App.svelte';
+import App_dev from './App_dev.svelte';
 
-const app = new App({
+const app = new App_dev({
   target: document.body,
 });
 
